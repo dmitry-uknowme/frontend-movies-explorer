137a30330e05d78fa85bde91deea91c7ed1398b9
t040496 <t040496@gmail.com>
2023-08-20 22:56:57 +0500

    fix

---
+++ src/pages/configs.ts
@@ -0,0 +1,21 @@
+export const xlScreenParams = {
+  initialCount: 16,
+  incrementByCount: 4,
+};
+
+export const lgScreenParams = {
+  initialCount: 12,
+  incrementByCount: 3,
+};
+
+export const mdScreenParams = {
+  initialCount: 8,
+  incrementByCount: 2,
+};
+
+export const smScreenParams = {
+  initialCount: 5,
+  incrementByCount: 2,
+};
+
+export const SHORT_MOVIE_LEN = 40;
